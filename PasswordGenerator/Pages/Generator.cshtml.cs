925774a4734fe8a99d264ab575996f9580181842
João Augusto <joaoaugusto7j@gmail.com>
2025-09-29 21:10:12 -0300

    feat: commit inicial com Generator Razor Page e estrutura do projeto

---
+++ PasswordGenerator/Pages/Generator.cshtml.cs
@@ -0,0 +1,47 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace PasswordGenerator.Pages
+{
+    public class GeneratorModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+
+        [BindProperty]
+        public int PasswordSize { get; set; }
+
+        [BindProperty]
+        public bool IncludeNumbers { get; set; }
+
+        [BindProperty]
+        public bool IncludeLowercase { get; set; }
+
+        [BindProperty]
+        public bool IncludeUppercase { get; set; }
+
+        [BindProperty]
+        public bool IncludeSpecial { get; set; }
+        public List<char> CharPool { get; set; } = new List<char>();
+
+        public string Generate()
+        {
+            CharPool.Clear();
+            if (IncludeNumbers) CharPool.AddRange("0123456789");
+            if (IncludeLowercase) CharPool.AddRange("abcdefghijklmnopqrstuvwxyz");
+            if (IncludeUppercase) CharPool.AddRange("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+            if (IncludeSpecial) CharPool.AddRange("!@#$%^&*()-_=+[]{}|;:,.<>?/");
+            StringBuilder password = new StringBuilder();
+
+            for (int i = 0; i < PasswordSize; i++)
+            {
+                int validPos = RandomNumberGenerator.GetInt32(CharPool.Count);
+                password.Append(CharPool[validPos]);
+            }
+            return password.ToString();
+        }
+    }
+}
